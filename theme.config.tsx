b9334402d263e1b63c39e57b839d811ccf677ca6
isamyakt <isamyakt17@gmail.com>
2023-05-05 16:22:37 +0530

    nextra init ✨

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <strong><span>Graux</span></strong>,
+  project: {
+    link: 'https://github.com/isamyakt/graux-docs.git',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/isamyakt/graux-docs.git',
+  footer: {
+    text: 'Graux Docs',
+  },
+}
+
+export default config
